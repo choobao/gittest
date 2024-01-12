aa54be437585245ee33883cf01c43167e5115bcf
승원 <153504415+bobbodaddeuk@users.noreply.github.com>
2024-01-12 22:27:25 +0900

    Update detail.js

--- detail.js
+++ detail.js
@@ -1,19 +1,33 @@
-const url =
-  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZTYzYjdjMWU5ZTIyZDAwM2ZjNjc5OWZlNjhmZWIxZCIsInN1YiI6IjY1OGViOWUxY2EwZTE3MDhmNGJhNzVkMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h4S8wIqJ0DxjJ9506Hrj3F5AfsavVajdG9cVybz_RnM",
-  },
-};
+import { options } from "./apikey.js";
+const url = "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
 
 document.addEventListener("DOMContentLoaded", function () {
   const searchParams = new URLSearchParams(window.location.search);
   const cardID = searchParams.get("id");
 
+  const userNickName = document.getElementById("nickName");
+  const userPassWord = document.getElementById("passWord");
+  const userSubmit = document.getElementById("submit");
+  const userReview = document.getElementById("review");
+
   APIid(cardID);
+  console.log(cardID);
+  userSubmit.addEventListener("click", () => {
+    let usersInfo = JSON.parse(localStorage.getItem(`movieId:${cardID}`)) || [];
+
+    let newUserInfo = {
+      nickname: userNickName.value,
+      passWord: userPassWord.value,
+      review: userReview.value
+    };
+
+    usersInfo.push(newUserInfo);
+
+    localStorage.setItem(`movieId:${cardID}`, JSON.stringify(usersInfo));
+    console.log(cardID);
+
+    location.reload();
+  });
 });
 
 function APIid(cardID) {
@@ -26,16 +40,16 @@ function APIid(cardID) {
       if (matchingData) {
         const title = matchingData["title"];
         const id = matchingData["id"];
-
+        const overview = matchingData["overview"];
         const vote = matchingData["vote_average"];
         const path = matchingData["poster_path"];
 
         let temp_html = `
-        <div class="card mb-3" id="card-${id}"  >
-        <img src="https://image.tmdb.org/t/p/w300${path}" class="card-img-top" alt="..." onclick="moveDetail(event)">
+        <div class="card mb-3" id="card-${id}"  onclick="moveDetail(event)">
+        <img src="https://image.tmdb.org/t/p/w300${path}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${title}</h5>
-         
+          <p class="card-text">${overview}</p>
           <p class="card-text"><small class="text-body-secondary">(${vote}/10)</small></p>
         </div>
       </div>`;
@@ -45,3 +59,9 @@ function APIid(cardID) {
       }
     });
 }
+
+//form태그로 인한 새로고침을 막고 alert창 띄우기
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("리뷰가 작성되었습니다.");
+});
